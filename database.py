e307daa0e2f13e00cc795cc37f6dda7ff813a36d
Giorgos Theo <giorgostheo97@gmail.com>
2020-09-25 14:48:05 +0300

    Changes folder structure and addeed separate todo file.

---
+++ database.py
@@ -0,0 +1,99 @@
+from __future__ import annotations
+import pickle
+
+class Database:
+    '''
+    Database class contains tables.
+    '''
+    def __init__(self):
+        self.tables = {}
+        self.len = 0
+
+    def _update(self):
+        '''
+        recalculate the number of tables in the Database
+        '''
+        self.len = len(self.tables)
+
+    def table_from_pkl(self, path):
+        '''
+        Load a table from a pkl file. tmp tables and saved tables are all saved using the pkl extention.
+        '''
+        new_table = Table(load=path)
+
+        if new_table.name not in self.__dir__():
+            setattr(self, new_table.name, new_table)
+        else:
+            raise Exception(f'"{new_table.name}" attribute already exists in "{self.__class__.__name__} "class.')
+
+        self.tables.update({new_table.name: new_table})
+        self._update()
+
+    def create_table(self, name=None, column_names=None, column_types=None, load=None):
+        '''
+        This method create a new table. This table is saved and can be accessed by
+        db_object.tables['table_name']
+        or
+        db_object.table_name
+        '''
+        self.tables.update({name: Table(name=name, column_names=column_names, column_types=column_types, load=load)})
+        # self.name = Table(name=name, column_names=column_names, column_types=column_types, load=load)
+        # check that new dynamic var doesnt exist already
+        if name not in self.__dir__():
+            setattr(self, name, self.tables[name])
+        else:
+            raise Exception(f'"{name}" attribute already exists in "{self.__class__.__name__} "class.')
+        # self.no_of_tables += 1
+        self._update()
+
+    def drop_table(self, table_name):
+        '''
+        Drop table with name 'table_name' from current db
+        '''
+        if self.tables[table_name]._is_locked():
+            print(f"!! Table '{table_name}' is currently locked")
+            return
+
+        del self.tables[table_name]
+        delattr(self, table_name)
+
+    def save(self, filename):
+        '''
+        Save db as a pkl file. This method saves the db object, ie all the tables and attributes.
+        '''
+        if filename.split('.')[-1] != 'pkl':
+            raise ValueError(f'ERROR -> Savefile needs .pkl extention')
+
+        with open(filename, 'wb') as f:
+            pickle.dump(self.__dict__, f)
+
+    def load(self, filename):
+        '''
+        Load a db from a saved db_object
+        '''
+        f = open(filename, 'rb')
+        tmp_dict = pickle.load(f)
+        f.close()
+
+        self.__dict__.update(tmp_dict)
+
+    def table_from_csv(self, filename, name=None, column_types=None):
+        '''
+        Create a table from a csv file.
+        If name is not specified, filename's name is used
+        If column types are not specified, all are regarded to be of type str
+        '''
+        if name is None:
+            name=filename.split('.')[:-1][0]
+
+        file = open(filename, 'r')
+
+        first_line=True
+        for line in file.readlines():
+            if first_line:
+                colnames = line.strip('\n').split(',')
+                self.create_table(name=name, column_names=colnames, column_types=[str for _ in colnames])
+                first_line = False
+                continue
+
+            self.tables[name].insert(line.strip('\n').split(','))
---
+++ misc.py
@@ -0,0 +1,13 @@
+import operator
+
+def get_op(op, a, b):
+    '''
+    Get op as a function of a and b by using a symbol
+    '''
+    ops = {'>': operator.gt,
+                '<': operator.lt,
+                '>=': operator.ge,
+                '<=': operator.le,
+                '==': operator.eq}
+
+    return ops[op](a,b)
