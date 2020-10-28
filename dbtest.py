9072d71f798efc6f21d6178f455e9419649e55ce
geo theo <giorgostheo97@gmail.com>
2020-10-28 17:09:43 +0200

    Cleaning up

--- dbtest.py
+++
@@ -1,5 +0,0 @@
-from database import Database
-
-db = Database('testdb')
-db.table_from_csv('students.csv', column_types=[str,int,str,int], primary_key='Student_id')
-db.create_index('students', 'id1')
