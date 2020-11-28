1b10d1a6922184b2a365d5bcd6dd787e94c14b27
gstheo <giorgostheo97@gmail.com>
2020-11-28 13:58:45 +0200

    MB to KB and added a try-except for the sys.argv

--- preview.py
+++ preview.py
@@ -37,7 +37,7 @@ def preview(dirname='dbdata', plot=False):
 
     total_db = sum(sizes)
     plt.annotate('Total size', xy=(-0.29, -0.0), size=16, color='darkred')
-    plt.annotate(str(int(round(total_db/1e6)))+' MB', xy=(-0.18,-0.15), size=15, color='#d62728')
+    plt.annotate(str(int(round(total_db/1e3)))+' KB', xy=(-0.18,-0.15), size=15, color='#d62728')
 
     centre_circle = plt.Circle((0,0),0.70,fc='white')
     fig = plt.gcf()
@@ -51,4 +51,7 @@ def preview(dirname='dbdata', plot=False):
         plt.show()
 
 if __name__ == '__main__':
-    preview(dirname=str(sys.argv[1]))
+    try:
+        preview(dirname=str(sys.argv[1]))
+    except IndexError:
+        print("ERROR: You needed to specify the folder that contains the databases or tables that you want to analyze.")
