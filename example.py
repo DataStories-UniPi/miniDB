ffd74e5e00878a63ca4c1891eb9cba8d3754d7e0
anampel <aanampel.am@gmail.com>
2021-01-14 00:57:32 +0200

    experiments

--- example.py
+++ example.py
@@ -1,12 +1,26 @@
 from database import Database
+
+
 # create db with name "example"
 db = Database('example', load=False)
 # create a single table named "myclass"
 db.create_table('myclass', ['col1', 'col2', 'col3'], [str,str,int])
+class Example:
 # insert 5 rows
-db.insert('myclass', ['M', '101', '500'])
-db.insert('myclass', ['A', '514', '10'])
-db.insert('myclass', ['P', '3128', '70'])
-db.insert('myclass', ['T', '100', '30'])
-db.insert('myclass', ['W', '120', '50'])
+	index=1
+
+	def imports(i):
+		if i == 0:
+			db.insert('myclass', ['A', '101', '500'])
+			db.insert('myclass', ['C', '514', '10'])
+			db.insert('myclass', ['F', '3128', '70'])
+			db.insert('myclass', ['L', '100', '30'])
+			db.insert('myclass', ['W', '120', '50'])
+		else:
+			i=1
+
+		return i
+	 
+	imports(index)
+	print (index)
 
---
+++ question1.py
@@ -0,0 +1,10 @@
+from database import Database 
+from example import Example
+
+# create db with name "question1"
+db = Database('question1', load=False)
+
+#create the insert stack
+insert_stack=[]
+
+print (insert_stack)
