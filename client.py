7c4383fb4a3a0d428bb12fb1d920c70cd0285cef
Marios Manis <marios-manis2000@hotmail.com>
2021-01-26 12:47:22 -0800

    Summary
    -Format code
    -Try...catch implementation at critical points
    - Provide explaining comments

--- client.py
+++ client.py
@@ -3,22 +3,25 @@ import sys
 import pickle
 from table import Table
 
+#client's network configuration
 target_host = sys.argv[1]
 target_port = 12345
 
+#initialize client's socket#
 #create a socket object.
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-#connect the client
-client.connect((target_host, target_port))
-
-
-print(client.recv(2048).decode())
-query = str(input("SQL query:"))
-#send some data.
-client.send(query.encode())
-
-response = pickle.loads(client.recv(2048))
-Table.show(response)
-
-client.close()
+try:
+    #connect the client socket
+    client.connect((target_host, target_port))
+    #receive welcome message from server
+    print(client.recv(2048).decode())
+    query = str(input("SQL query:"))
+    #send some data.
+    client.send(query.encode())
+    #print query's result to the client
+    response = pickle.loads(client.recv(2048))
+    Table.show(response)
+    #close the client socket
+    client.close()
+except Exception as e:
+    print(str(e))
--- server.py
+++ server.py
@@ -8,44 +8,47 @@ host = "127.0.0.1"
 port = 12345
 
 #initialize server's socket#
-
 #server's socket is an IPv4 socket with TCP stream
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-#bind network's configuration with the created socket
-server.bind((host, port))
-#set server's socket in listening mode
-server.listen(5)
+try:
+    #bind network's configuration with the created socket
+    server.bind((host, port))
+    #set server's socket in listening mode
+    server.listen(5)
+    #print server's status
+    print("[*] Server is listening on %s:%d..." % (host, port))
+    #load database
+    db = Database("smdb", load=True)
+except Exception as e:
+    client.send(str(e).encode())
 
-#print server's status
-print("[*] Server is listening on %s:%d..." % (host, port))
-
-db = Database("smdb", load=True)
 
 #function to handle a client
 def handle_client(client_socket):
     #client's query
-    client_query = client_socket.recv(1024).decode()
-    db_result = db.select(client_query.split(" ")[3], client_query.split(" ")[1], return_object=True)
-    pickle_obj = pickle.dumps(db_result)
-    client.send(pickle_obj)
-    #print(pickle_obj)
-    client_socket.close()
-    
-# SELECT * FROM table_name
-        
+        client_query = client_socket.recv(1024).decode()      
+        try:
+            #example query format: SELECT * FROM TABLE_NAME
+            db_result = db.select(client_query.split(" ")[3], client_query.split(" ")[1], return_object=True)
+            #send query results to the client
+            client.send(pickle.dumps(db_result))
+            #close connection with client
+            client_socket.close()
+        except Exception as e:
+            client.send(str(e).encode())
+            
 while True:
-    #accept client's connection
-    client, addr = server.accept()
-
-    #welcome message to client and request a query
-    client.send("[*] Hello please insert a query".encode())
-
-    #print client's socket info
-    print("Accepted connection from -> %s:%d" % (addr[0],addr[1]))
-    
-    #create a thread for the client and call handle_client function to handle the client
-    client_handler = threading.Thread(target= handle_client(client), args= (client, ))
-    
-    #start client's thread
-    client_handler.start()
+    try:
+        #accept client's connection
+        client, addr = server.accept()
+        #welcome message to client and request a query
+        client.send("[*] Hello please insert a query".encode())
+        #print client's socket info
+        print("Accepted connection from -> %s:%d" % (addr[0],addr[1]))
+        #create a thread for the client and call handle_client function to handle the client
+        client_handler = threading.Thread(target= handle_client(client), args= (client, ))      
+        #start client's thread
+        client_handler.start()
+    except Exception as e:
+        client.send(str(e).encode())
