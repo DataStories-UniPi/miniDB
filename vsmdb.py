a23dd6da879070c9c1fe68d95822c27b0e7bc377
DimKoromilas <cslab010@cslab010-MS-7588>
2021-02-21 02:00:12 +0200

    test change vsmdb.py

--- vsmdb.py
+++ vsmdb.py
@@ -6,4 +6,3 @@ db.create_table('classroom', ['building', 'room_number', 'capacity'], [str,str,i
 # insert 5 rows
 db.insert('classroom', ['Packard', '101', '500'])
 db.insert('classroom', ['Painter', '514', '10'])
-db.insert('classroom', ['Taylor', '3128', '70'])
