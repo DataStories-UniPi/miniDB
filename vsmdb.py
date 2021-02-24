4e218239d0c7ca4bcefb8a8801cd362439d38b30
Dimitris Koromilas <79376107+DimKoromilas@users.noreply.github.com>
2021-02-24 02:10:36 +0200

    Delete vsmdb.py

--- vsmdb.py
+++
@@ -1,7 +0,0 @@
-from database import Database
-# create db with name "smdb"
-db = Database('vsmdb', load=False)
-# create a single table named "classroom"
-db.create_table('classroom', ['building', 'room_number', 'capacity'], [str,str,int])
-# insert 5 rows
-db.insert('classroom', ['Packard', '101', '500'])
