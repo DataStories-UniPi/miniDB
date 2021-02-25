6394bb1e8b4f279814c1bbe1129429fa15f15d31
Dimitris Koromilas <79376107+DimKoromilas@users.noreply.github.com>
2021-02-25 15:08:07 +0200

    Update misc.py

--- misc.py
+++ misc.py
@@ -4,14 +4,14 @@ def get_op(op, a, b):
     '''
     Get op as a function of a and b by using a symbol
     '''
-    ops = {     
-                '>': operator.gt,
-                '<': operator.lt,
-                '>=': operator.ge,
-                '<=': operator.le,
-                '==': operator.eq,
-                '!=': operator.ne
-           }
+    ops = { 
+            '>': operator.gt,
+            '<': operator.lt,
+            '>=': operator.ge,
+            '<=': operator.le,
+            '==': operator.eq,
+            '!=': operator.ne
+          }
 
     try:
         return ops[op](a,b)
