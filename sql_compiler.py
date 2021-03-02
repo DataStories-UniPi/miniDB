bfe42eeee7fa47727a62bc61ece829c75171eb15
Chris Ster <stergioy.christos@gmail.com>
2021-03-02 12:22:41 +0200

    Basic functionality

--- sql_compiler.py
+++ sql_compiler.py
@@ -1,57 +1,15 @@
 from database import Database
-import vsmdb
 import re
-#vsmdb.db.select('classroom', ['building', 'capacity'])
-searchquery = "select * from classroom" #input("Enter a command to execute:")
-updatequery = "update classroom set capacity=39 where room_number==120"
-createdbquery = "create database test_db"
-createtbquery = "create table professors1(salary int, name str, age int)"
-droptbquery = "drop table professors1"
-insertquery = "insert into professors1 values (50, takhs, 87)"
-searchregex = "(select) (\*?|([A-Za-z_]+,?\s?)+) (from) ([A-Za-z_]+)?"
-updateregex = "(update)\s([A-Za-z0-9_]+)\s(set)\s(([A-Za-z0-9_]+)[<=>]*([A-Za-z0-9_]+))\s(where)\s([A-Za-z0-9_]+[<=>]*[A-Za-z0-9_]+)"
-createdbregex = "(create)\s(database)\s([A-Za-z_0-9]+)"
-createtbregex = "(create)\s(table)\s([A-Za-z_0-9]+)\(([A-za-z_0-9]+\s(str|int)(,\s[A-za-z_0-9]+\s(str|int))*)\)"
-droptbregex = "(drop)\s(table)\s([A-Za-z_0-9]+)"
-insertregex = "(insert)\s(into)\s([A-Za-z0-9_]+)\s(values)\s?\([A-Za-z0-9_]+(,\s[A-za-z_0-9]+)*\)"
-splitsearch = re.split(searchregex, searchquery)
-splitupdate = re.split(updateregex, updatequery)
-splitcreatedb = re.split(createdbregex, createdbquery)
-splitcreatetb = re.split(createtbregex, createtbquery)
-splitdroptb = re.split(droptbregex, droptbquery)
-splitinsert = re.split(insertregex, insertquery)
-print(splitsearch)
-splitargs = splitsearch[2].split(", ")
-print(splitargs)
-for arguments in splitargs:
-    print (arguments)
-print(splitupdate)
-print(splitcreatedb)
-print(splitcreatetb)
-print(splitdroptb)
-print(splitinsert)
-if(splitsearch[1] == "select" and splitsearch[2] == '*'):
-    vsmdb.db.select(splitsearch[5], splitsearch[2])
-elif(splitsearch[1] == "select" and splitsearch[2] != '*'):
-    vsmdb.db.select(splitsearch[5], splitargs)
-if(splitupdate[1] == "update"):
-    vsmdb.db.update(splitupdate[2], int(splitupdate[6]), splitupdate[5], splitupdate[8])
-vsmdb.db.select('classroom', '*')
-if(splitcreatedb[1] == "create" and splitcreatedb[2] == "database"):
-    dbname = splitcreatedb[3]
-    testdb = Database(dbname, load = False)
-    testdb.create_table('test_table', ['name', 'height', 'weight'], [str, int, int])
-    testdb.insert('test_table', ['Bob', 190, 80])
-    testdb.insert('test_table', ['Patric', 170, 60])
-    testdb.select('test_table', '*')
-if(splitcreatetb[1] == "create" and splitcreatetb[2] == "table"):
-    cleanstring = splitcreatetb[4].replace(',', '')
-    tbargs = cleanstring.split(" ")
+import sys
+
+def create_table_func(querystring): #Function to create a table based on the query
+    createtbregex = "(create)\s(table)\s([A-Za-z_0-9]+)\(([A-za-z_0-9]+\s(str|int)(,\s[A-za-z_0-9]+\s(str|int))*)\)"
+    splitcreatetb = re.split(createtbregex, querystring) #Get a list whose elements are the matched patterns from the query and the regular expression made to detect a create sql command
+    cleanstring = splitcreatetb[4].replace(',', '') #Trim the element contaning the column name and column types so their values can be inserted in to two different lists
+    tbargs = cleanstring.split(" ")#Seperate the name of the column from the type of the column
     columns = []
     column_type = []
-    print(tbargs)
-    for i in range(len(tbargs)):
-        print(tbargs[i])
+    for i in range(len(tbargs)):#Append the name or the type of the column to the approptiate list
         if (i % 2 == 0):
             columns.append(tbargs[i])
         else:
@@ -59,16 +17,112 @@ if(splitcreatetb[1] == "create" and splitcreatetb[2] == "table"):
                 column_type.append(str)
             elif(tbargs[i] == "int"):
                 column_type.append(int)
-    print(columns)
-    print(column_type)
-    testdb.create_table(splitcreatetb[3], columns, column_type)
-    testdb.insert('professors1', [100, 'Patric', 47])
-    testdb.insert('professors1', [170, 'Bob', 35])
-    testdb.tables[splitcreatetb[3]]
-    testdb.select('professors1', '*')
-if(splitdroptb[1] == "drop" and splitdroptb[2] == "table"):
-    testdb.drop_table(splitdroptb[3]) 
-#(select)\s*(\*?|([A-Za-z_]+,?\s?)+)\s*(from)\s*([A-Za-z_]+)\s*(where [A-Za-z_]+(<=|<|==|>=|>\d+))?\s*(top=(\d)+)?\s*(order by ([A-Za-z_]+ (asc|desc)))?\s*(save as ([A-Za-z_]+)+)?
-#safe (select) (\*?|([A-Za-z_]+,?\s?)+) (from) ([A-Za-z_]+)
-#createtable (create)\s(table)\s([A-Za-z_0-9]+)\(([A-za-z_0-9]+\s(str|int)(,\s[A-za-z_0-9]+\s(str|int))*)\)
-#insert (insert)\s(into)\s([A-Za-z0-9_]+)\s(values)\s\([A-Za-z0-9_]+(,\s[A-za-z_0-9]+)*\)
+    try:
+        db.create_table(splitcreatetb[3], columns, column_type)
+    except:
+        print("Table creation was not successfull!")
+
+def insert_func(querystring):
+        insertregex = "(insert)\s(into)\s([A-Za-z0-9_]+)\s(values)\s?\(([A-Za-z0-9_]+(,\s[A-za-z_0-9]+)*)\)"
+        splitinsert = re.split(insertregex, querystring)
+        cleanstring = splitinsert[5].replace(',', '')
+        splitargs = cleanstring.split(" ")
+        try:
+            db.insert(splitinsert[3], splitargs)
+            print(f"Inserted row {splitargs} in table {splitinsert[3]}")
+        except:
+            print("Insertion command was not successfull!")
+
+db = Database("ExampleDB", load=False)
+create_table_func("create table sampleppl(name str, height int, weight int, age int)")
+insert_func("insert into sampleppl values (Jim, 190, 90, 30)")
+insert_func("insert into sampleppl values (Anna, 167, 58, 25)")
+print("A sample database has been created with name 'ExampleDB'\none table named 'sampleppl' with the following columns(name str, height int, age int).\n", 
+"and the following rows:\n",
+"row1 (Jim, 190, 90, 30)\n",
+"row2 (Anna, 167, 58, 25)")
+print("Switch to another existing database using 'load database database_name' command.")
+while (True):
+    query = input("Enter an sql query to be executed or 'exit' to quit: \n")
+    if(query.split(" ")[0] == ("select")):
+        #selectregex = (select)\s*(\*?|([A-Za-z_]+,?\s?)+)\s*(from)\s*([A-Za-z_]+)\s*(where [A-Za-z_]+(<=|<|==|>=|>\d+))?\s*(top=(\d)+)?\s*(order by ([A-Za-z_]+ (asc|desc)))?\s*(save as ([A-Za-z_]+)+)?
+        selectregex = "(select) (\*?|([A-Za-z_]+,?\s?)+) (from) ([A-Za-z_]+)?"
+        splitselect = re.split(selectregex, query)
+        if(splitselect[1] == "select" and splitselect[2] == '*'):
+            try:
+                db.select(splitselect[5], splitselect[2])
+            except:
+                print("Columns could not be selected due to an error!")
+        elif(splitselect[1] == "select" and splitselect[2] != '*'):
+            splitargs = splitselect[2].split(", ")
+            try:
+                db.select(splitselect[5], splitargs)
+            except:
+                print("Columns could not be selected due to an error!")
+    elif(query.split(" ")[0] == ("update")):
+        updateregex = "(update)\s([A-Za-z0-9_]+)\s(set)\s(([A-Za-z0-9_]+)[<=>]*(\"?[A-Za-z0-9_]+\"?))\s(where)\s([A-Za-z0-9_]+[<=>]*[A-Za-z0-9_]+)"
+        splitupdate = re.split(updateregex, query)
+        trimsplit1 = splitupdate[6].replace("\"",'')
+        trimsplit2 = splitupdate[8].replace("\"", '')
+        if('\"' in splitupdate[6]):
+            try:
+                db.update(splitupdate[2], trimsplit1, splitupdate[5], trimsplit2)
+                print(f"Column {splitupdate[5]} has been updated with value: {trimsplit1}")
+            except:
+                print("Update query was not successfull!")
+        else:
+            try:
+                db.update(splitupdate[2], int(trimsplit1), splitupdate[5], trimsplit2)
+                print(f"Column {splitupdate[5]} has been updated with value: {trimsplit1}")
+            except:
+                print("Update query was not successfull")
+    elif(query.split(" ")[0] == "create" and query.split(" ")[1] == "database"):
+        createdbregex = "(create)\s(database)\s([A-Za-z_0-9]+)"
+        splitcreatedb = re.split(createdbregex, query)
+        dbname = splitcreatedb[3]
+        try:
+            db = Database(dbname, load = False)
+            print(f"Database {dbname} was created.")
+        except:
+            print("Database creation was not successfull!")
+    elif(query.split(" ")[0] == "create" and query.split(" ")[1] == "table"):
+        create_table_func(query)
+    elif(query.split(" ")[0] == "insert"):
+        insert_func(query)
+    elif(query.split(" ")[0] == "delete"):
+        deleteregex = "(delete)\s(from)\s([A-Za-z0-9_]+)\s(where)\s([A-za-z0-9_]+\s?[<=>]*\s?[A-za-z0-9_]+)"
+        splitdelete = re.split(deleteregex, query)
+        try:
+            db.delete(splitdelete[3], splitdelete[5])
+        except:
+            print("Deletion was not successfull!")
+    elif(query.split(" ")[0] == "drop" and query.split(" ")[1] == "table"):
+        droptbregex = "(drop)\s(table)\s([A-Za-z_0-9]+)"
+        splitdroptb = re.split(droptbregex, query)
+        try:
+            db.drop_table(splitdroptb[3])
+            print(f"Table {splitdroptb[3]} was dropped.")
+        except:
+            print("Table could not be dropped due to an error!")
+    elif(query.split(" ")[0] == "load" and query.split(" ")[1] == "database"):
+        loaddbregex = "(load)\s(database)\s([A-Za-z0-9_]+)"
+        splitloaddb = re.split(loaddbregex, query)
+        try:
+            db = Database(splitloaddb[3], load = True)
+        except:
+            print("Database could not be loaded due to an error!")
+    elif(query.split(" ")[0] == "help"): # Print a help message for the user to see
+        print("Currently supported sql commands are: \n ",
+        "select columns/* from table_name\n",
+        "update table_name set value where condition\n",
+        "insert into table_name values (value1, value2, ...)\n",
+        "delete from table_name where condition\n",
+        "create table table_name(column_name1 column_type1, column_name2 column_type2, ...)\n",
+        "drop table table_name\n",
+        "create database database_name\n",
+        "load database database_name"
+        )
+    elif(query.split(" ")[0] == "exit"):
+        sys.exit(0)
+    else:
+        print("Invalid sql syntax!\ntype 'help' to see a list of available supported commands and their syntax or 'exit' to quit.")
