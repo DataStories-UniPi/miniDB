e1206902f7b46e75f4f37d84a21b61d8704ff498
Bill <bargiropoulos@gmail.com>
2021-10-24 18:11:21 +0300

    Fixes old SQLINTERPRETER - BILL

--- src/insert/srv.py
+++ src/insert/srv.py
@@ -1,5 +1,12 @@
+import os, sys
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+
 import socket
-import sqlInterpreter
+from sqlInterpreter import SqlInterpreter
+
+from db.database import Database
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
@@ -8,13 +15,14 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
     conn, addr = s.accept()
+    inter = SqlInterpreter(Database)
     with conn:
         print('Connected by', addr)
         while True:
             data = conn.recv(1024)
             data = data.decode('utf-8')
             #send_query(data)
-            a = str(sqlInterpreter.main(query=data))
+            a = str(inter.interpret(data))
             if data == 'exit()':
                 print('done')
                 break
