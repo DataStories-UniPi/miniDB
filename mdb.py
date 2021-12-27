b0b65618432ab0124ee110a92d34c79974818a3f
NickGeo1 <60719667+NickGeo1@users.noreply.github.com>
2021-12-27 03:46:01 +0200

    Προστεθηκαν κιαλα σχολια
    Προστεθηκαν σχολια για ολη την evaluate_from_clause.
    Βλεπε Update 1 στο art
    Εχουν προστεθει αρκετα παραδειγματα για κατανοηση

--- mdb.py
+++ mdb.py
@@ -25,9 +25,19 @@ TO PROGRAMMA PAIRNEI SAN INPUT TO QUERY TOY XRHSTH.
 STH SYNEXEIA ELEGXEI TO PRWTO KEYWORD. TO KATHE KEYWORD THS SQL
 EXEI ALLA PROVLEPOMENA KEYWORDS POU MPOREI NA AKOLOYTHHSOYN.
 AYTA TA PROVLEPOMENA KEYWORDS ELEGXETAI AN YPARXOYN MESA SE OLO TO QUERY
-POU EDWSE O XRHSTHS. OSA YPARXOYN ONTWS APOTHIKEVONTAI SE LISTA/LEKSIKO.
+POU EDWSE O XRHSTHS. OSA YPARXOYN ONTWS APOTHIKEVONTAI SE LISTA.
 SE ENA LEKSIKO TELIKA EXOUME TA KEYWORDS MAZI ME AYTA POY EKTELOUN
 PX {'select': '*', 'from': 'table', ...}
+
+UPDATE 1: ENA LEKSIKO ANTIPROSWPEVEI ENA QUERY.
+select * from table1 where id=100; --> {'select': '*', 'from': 'table1', 'where': 'id=100'}
+
+EXOUME EPISHS NESTED QUERIES
+select * from (select * from course) inner join teaches on course_id=course_id;
+{'select': '*', 'from': {'left': {'select': '*', 'from': 'course', 'order by': None, 'top': None, 'desc': None}, 'right': 'teaches', 'join': 'inner', 'on': 'course_id=course_id'}, 'where': None,'order by': None, 'top': None, 'desc': None}
+
+ 
+ 
 
 """
 
@@ -69,7 +79,6 @@ def create_query_plan(query, keywords, action):
         elif i!=len(ql)-1 and f'{ql[i]} {ql[i+1]}' in keywords and not in_paren(ql, i): #an i-osth kai (i+1)-osth leksh tou query apotelei keyword kai den einai se parenthesh
             kw_in_query.append(f'{ql[i]} {ql[i+1]}')
             kw_positions.append(i+1) #apothikevoyme to index ths deyterhs lekshs toy keyword
-
 
     for i in range(len(kw_in_query)-1):
         dic[kw_in_query[i]] = ' '.join(ql[kw_positions[i]+1:kw_positions[i+1]]) #ME EKAPSES THEODOROPOYLE, ANTE NA TO EKSHGHSW AYTO
@@ -134,36 +143,69 @@ def evaluate_from_clause(dic):
     '''
     Evaluate the part of the query (argument or subquery) that is supplied as the 'from' argument
     '''
-    join_types = ['inner', 'left', 'right', 'full']
-    from_split = dic['from'].split(' ')
-    if from_split[0] == '(' and from_split[-1] == ')':
-        subquery = ' '.join(from_split[1:-1])
-        dic['from'] = interpret(subquery)
+    join_types = ['inner', 'left', 'right', 'full'] #se auto to kommati to dic['from'] mporei na exei kapoio join h oxi
+                                                    #px select * from t1 inner join t2 on c1=c2, sto leksiko tha exoume apo to t1 kai meta
+                                                    #px select * from t1 where c1=10, sto leksiko tha exoyme to 't1'
+        
+    from_split = dic['from'].split(' ') #vazoume oles tis lekseis meta to from kai prin to epomeno keyword se lista
+    if from_split[0] == '(' and from_split[-1] == ')': # sth periptwsh poy exoyme nested query meta to from
+        subquery = ' '.join(from_split[1:-1]) #pairnoume tis lekseis toy subquery sth lista kai tis enwnoume me keno
+        dic['from'] = interpret(subquery) #ginetai analysh tou subquery.(vlepw to action klp me thn interpret) sto value tou key 'from' tha exw pleon leksiko
 
-    join_idx = [i for i,word in enumerate(from_split) if word=='join' and not in_paren(from_split,i)]
-    on_idx = [i for i,word in enumerate(from_split) if word=='on' and not in_paren(from_split,i)]
-    if join_idx:
-        join_idx = join_idx[0]
-        on_idx = on_idx[0]
-        join_dic = {}
-        if from_split[join_idx-1] in join_types:
-            join_dic['join'] = from_split[join_idx-1]
-            join_dic['left'] = ' '.join(from_split[:join_idx-1])
+    join_idx = [i for i,word in enumerate(from_split) if word=='join' and not in_paren(from_split,i)] #pairnoume ola ta indexes twn 'join' an auta den einai se parenthesh
+    on_idx = [i for i,word in enumerate(from_split) if word=='on' and not in_paren(from_split,i)] #pairnoume ola ta indexes twn 'on' an ayta den einai se parenthesh
+    '''
+    to enumerate(from_split) periexei tuples me thn arithmish tou kathe stoixeiou ths listas
+    px an from_split=['t2,', 't1'] tote list(enumerate(from_split)) = [(0,'t2,'),(1,'t1')]
+    
+    NA SHMEIWTHEI(h akolouthh syntaksh einai mono gia thn miniDB):
+    
+    -Einai pithano na exoume nested query kai join:
+    px select * from (select * from table1) inner join table2 on common=common
+    (se alles sql prepei na valoume 'as t1' meta to nested kai na prosdiorisoume ta tables twn common)
+    
+    -Den ypostirizontai pollapla joins:
+    px SELECT customerName, customercity, customermail, salestotal
+    
+    FROM onlinecustomers                FROM onlinecustomers
+    INNER JOIN                          INNER JOIN
+    orders                              (orders
+    ON customerid = customerid    H     INNER JOIN
+    INNER JOIN                          sales
+    sales                               ON orderId = orderId)
+    ON orderId = orderId                ON customerid = customerid
+    
+    Oi parapanw periptwseis einai diaforetikes
+    '''
+    if join_idx: #an uparxoun join genika
+        join_idx = join_idx[0] #thesh toy prwtou 'join' sth lista poy periexei tis lekseis meta to from
+        on_idx = on_idx[0] #thesh toy prwtou 'on' sth lista poy periexei tis lekseis meta to from
+        join_dic = {} 
+        '''
+        to leksiko sto telos periexei san keys th leksh 'join', 'left', 'right' kai 'on'. Ta values toys mas lene
+        ton typo toy join, ton pinaka pou yparxei aristera toy join, ton pinaka poy yparxei deksia toy join kai 
+        th synthkh tou 'on' antistoixa. Ta values sta left kai right mporei na einai leksika an exoume nested queries 
+        '''
+        if from_split[join_idx-1] in join_types: #an vreis join type amesws prin th leksh 'join'
+            join_dic['join'] = from_split[join_idx-1] #prosthese sto leksiko san key to 'join' me value to typo toy join
+            join_dic['left'] = ' '.join(from_split[:join_idx-1]) #prosthese sto leksiko san key to 'left' me value olo to keimeno meta to 'from' kai prin to join type.Ayto apotelei to aristero table toy join
         else:
-            join_dic['join'] = 'inner'
-            join_dic['left'] = ' '.join(from_split[:join_idx])
-        join_dic['right'] = ' '.join(from_split[join_idx+1:on_idx])
-        join_dic['on'] = ''.join(from_split[on_idx+1:])
+            join_dic['join'] = 'inner' #alliws an den anaferetai typos, to sketo 'join' einai to idio me to 'inner join'
+            join_dic['left'] = ' '.join(from_split[:join_idx]) #prosthese sto leksiko san key to 'left' me value olo to keimeno meta to 'from' kai prin to 'join'.Ayto apotelei to aristero table toy join
+        join_dic['right'] = ' '.join(from_split[join_idx+1:on_idx]) #prosthese sto leksiko san key to 'right' me value olo to keimeno meta to 'join' kai prin to 'on'.Ayto apotelei to deksio pinaka toy join
+        join_dic['on'] = ''.join(from_split[on_idx+1:]) #prosthese sto leksiko san key to 'on' me value olo to keimeno meta to 'on' kai prin to epomeno keyword. Ayto apotelei th sthlh poy tha ginei to join
 
         if join_dic['left'].startswith('(') and join_dic['left'].endswith(')'):
-            join_dic['left'] = interpret(join_dic['left'][1:-1].strip())
-
+            join_dic['left'] = interpret(join_dic['left'][1:-1].strip()) #an o aristeros pinakas tou join perikleietai apo parentheseis(isws einai nested query) kane interpret ayto to query
+                                                                         #pleon to key 'left' tha exei value ena leksiko
         if join_dic['right'].startswith('(') and join_dic['right'].endswith(')'):
-            join_dic['right'] = interpret(join_dic['right'][1:-1].strip())
-
-        dic['from'] = join_dic
-        
-    return dic
+            join_dic['right'] = interpret(join_dic['right'][1:-1].strip()) #an o deksios pinakas tou join perikleietai apo parentheseis(isws einai nested query) kane interpret ayto to query
+                                                                           #pleon to key 'right' tha exei value ena leksiko
+              
+        dic['from'] = join_dic #edw to value toy dic me key 'from' pauei na periexei oles tis lekseis meta to from kai prin to epomeno keyword.
+                               #pleon periexei san value to leksiko join_dic poy perigrafei toys pinakes aristera kai deksia toy join kai to typo you join
+          
+    return dic #edw epistrefetai to dic poy pleon sto 'from' periexei leksiko. An to query den eixe 'join' tote to dic den allazei
 
 def interpret(query):
     '''
@@ -204,7 +246,7 @@ def execute_dic(dic):
             dic[key] = execute_dic(dic[key])
     
     action = list(dic.keys())[0].replace(' ','_')
-    return getattr(db, action)(*dic.values())
+    return getattr(db, action)(*dic.values())g
 
 def interpret_meta(command):
     """
