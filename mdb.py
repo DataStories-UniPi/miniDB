6284bf61f67c80896270a1f07d3e8934739e58b9
Nassosvit <nassos1944@hotmail.com>
2022-01-10 14:34:53 +0200

    random removed

--- mdb.py
+++ mdb.py
@@ -9,7 +9,6 @@ sys.path.append('miniDB')
 
 from database import Database
 from table import Table
-from random import random
 # art font is "big"
 art = '''
              _         _  _____   ____  
