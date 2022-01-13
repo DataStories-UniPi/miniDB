4f524de102d620b50299bc97dc4a781170292012
skarts1908 <75524637+skarts1908@users.noreply.github.com>
2022-01-13 12:03:24 +0200

    Update database.py with in,between,like
    Update of database.py with addition of in, between, like in the condition check.

--- miniDB/database.py
+++ miniDB/database.py
@@ -338,7 +338,10 @@ class Database:
             return table_name._select_where(columns, condition, order_by, desc, top_k)
 
         if condition is not None:
-            condition_column = split_condition(condition)[0]
+            if "in" in condition.split() or "between" in condition.split() or "like" in condition.split():
+                condition_column = condition.split(" ")[0]
+            else:
+                condition_column = split_condition(condition)[0]
         else:
             condition_column = ''
 
