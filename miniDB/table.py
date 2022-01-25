44e2b68a617042988344339dc2eb3342df89d03b
Stratis Dermanoutsos <stratis450@gmail.com>
2022-01-25 20:49:47 +0200

    Finished NOT NULL constraint

--- miniDB/table.py
+++ miniDB/table.py
@@ -137,6 +137,10 @@ class Table:
             raise ValueError(f'ERROR -> Cannot insert {len(row)} values. Only {len(self.column_names)} columns exist')
 
         for i in range(len(row)):
+            # first check if the value can be null and if it is
+            if self.column_names[i] in self.not_null_columns and row[i] == '':
+                raise ValueError(f'ERROR -> Cannot insert NULL value in column "{self.column_names[i]}"')
+
             # for each value, cast and replace it in row.
             # try:
             row[i] = self.column_types[i](row[i])
