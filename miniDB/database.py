35ee6f5ccdc94ecfe720b87ac5e648ce543f5795
Magkasv2001 <magkasv2001@localhost.localdomain>
2022-02-12 22:37:45 +0200

    More fixes for drop trigger

--- miniDB/database.py
+++ miniDB/database.py
@@ -139,7 +139,7 @@ class Database:
         for row in self.tables['meta_triggers'].data: #for all rows of meta_triggers...
          if row[0]==trigger_name: #check if there is trigger with name same as the input
           self.delete_from('meta_triggers', f'trigger_name={trigger_name}') #drop trigger(replace attributes witn Nones)
-          count++
+          count+=1
         if(count==0):
          print('No trigger with that name exists')
 
