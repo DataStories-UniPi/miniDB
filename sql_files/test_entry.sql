36b1eee39495c06e341f17b80e563c039c29f8bc
Sophia Panousi <sophiapanousi@gmail.com>
2022-02-13 10:47:28 +0200

    last touch

--- sql_files/test_entry.sql
+++ sql_files/test_entry.sql
@@ -1,4 +1,4 @@
 create table student2 (ID str primary key, name str, dept_name str, tot_cred int);
-insert into student2 values (13121,Foureira,Finance,40);
+insert into student2 values (13121,Panopoulos,Finance,40);
 insert into student2 values (19141,Panousi,Finance,60);
 create table student3 (ID str primary key, name str, dept_name str,instructor str, tot_cred int, foreign key dept_name references department(dept_name),foreign key instructor references instructor(ID));
