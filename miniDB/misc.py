1f484d7d381a95bc101c8034ef63c4501e964319
Stratis Dermanoutsos <stratis450@gmail.com>
2022-08-05 21:15:31 +0300

    code cleanup

--- miniDB/misc.py
+++ miniDB/misc.py
@@ -27,18 +27,12 @@ def split_condition(condition):
         if len(splt)>1:
             left, right = splt[0].strip(), splt[1].strip()
 
-            if right[0] == '"' == right[-1]:
+            if right[0] == '"' == right[-1]: # If the value has leading and trailing quotes, remove them.
                 right = right.strip('"')
+            elif ' ' in right: # If it has whitespaces but no leading and trailing double quotes, throw.
+                raise ValueError(f'Invalid condition: {condition}\nValue must be enclosed in double quotation marks to include whitespaces.')
 
-                if right.find('"') != -1:
-                    raise ValueError(f'Invalid condition: {condition}. Double quotation marks are not allowed inside values.')
+            if right.find('"') != -1: # If there are any double quotes in the value, throw. (Notice we've already removed the leading and trailing ones)
+                raise ValueError(f'Invalid condition: {condition}\nDouble quotation marks are not allowed inside values.')
 
-                return left, op_key, right
-            else:
-                if right.find('"') != -1:
-                    raise ValueError(f'Invalid condition: {condition}. Double quotation marks are not allowed inside values.')
-
-                if ' ' in right:
-                    raise ValueError(f'Invalid condition ({condition}). Value must be enclosed in double quotation marks to include whitespaces.')
-                else:
-                    return left, op_key, right
+            return left, op_key, right
