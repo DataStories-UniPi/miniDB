424b68008bff86954e0b53b5c863fdce1cadbbd7
artemisoikonomou <59887252+artemisoikonomou@users.noreply.github.com>
2022-08-28 13:56:26 +0300

    Update database.py

--- miniDB/database.py
+++ miniDB/database.py
@@ -54,6 +54,21 @@ class Database:
         self.create_table('meta_indexes', 'table_name,index_name', 'str,str')
         self.save_database()
 
+         #pollapla insert se pinaka gia to issue 78
+ def multieinsert(self, table_name, select_query, lock_load_save=True):
+ try:
+  db = Database(self._name, load=True)
+  select_query = select_query.replace(' ', '') # remove all whitespaces
+ a = '''insert_data = db''' + select_query[len(select_query.split('.')[0]):] + '''.data'''
+ loc = {}
+ exec(a, {'db.select': db.select, 'db': db}, loc)
+ insert_data = loc['insert_data']
+ print('rows: ', insert_data)
+ for row in insert_data:
+ self.insert(table_name, row, lock_load_save)
+ except Exception as e:
+ print(e)
+ print('ABORTED')
     def save_database(self):
         '''
         Save database as a pkl file. This method saves the database object, including all tables and attributes.
