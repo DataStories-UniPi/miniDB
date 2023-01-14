41104a6d5432e0a612fc7c2ae77b54121e8461d6
Aimilios Kourpas <emil.pedos@gmail.com>
2023-01-14 14:52:37 +0200

    as elpisoume oti teliwse auto to thema

--- miniDB/table.py
+++ miniDB/table.py
@@ -602,9 +602,6 @@ class Table:
         tmp_dict = pickle.load(f)
         f.close()
 
-
         self.__dict__.update(tmp_dict.__dict__)
 
-    #Auto einai ena testaki 
-    #allo ena 
-    #kai ena allo
+    
