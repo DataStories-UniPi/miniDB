4c53010086ce1989eedf3566dbaad7068380c169
theodorakaika <theodkaika10@outlook.com.gr>
2023-02-05 21:23:08 +0200

    Creation of bucket class hash function

--- miniDB/hash.py
+++ miniDB/hash.py
@@ -18,3 +18,19 @@ class Hash:
             b = Bucket(global_depth)
             self.buckets.append(b)
             self.dict[i] = b
+
+#class for buckets creation
+class Bucket:
+    def __init__(self, local_depth):
+        self.data = []
+        self.local_depth = local_depth
+
+    def __str__(self):
+        return "[" + str(self.local_depth) + "] ... [" + ' '.join(str(e) for e in self.data) + "]"
+
+#hash function
+def hash_func(value, m):
+    hash_object = hashlib.sha1(bytes(str(value), encoding='utf-8'))
+    hex_dig = hash_object.hexdigest()
+    res = int(hex_dig, 32)
+    return res % m
