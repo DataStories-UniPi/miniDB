a9b8d62dfcab6f05b9d1271a942583d43bf3996c
sylvia-giagkou <sylvia.yiag@gmail.com>
2023-02-05 21:02:59 +0200

    class hash def init

--- miniDB/hash.py
+++ miniDB/hash.py
@@ -1,1 +1,20 @@
+import hashlib
 
+
+#import libary hashlib
+#Create class Hash
+#Create initial state of hash index.
+class Hash:
+    def __init__(self, global_depth, max_bucket=4):
+        '''
+        The Hash abstraction.
+        '''
+        self.dict = {}
+        self.buckets = []
+        self.global_depth = global_depth
+        self.bucket_max_size = max_bucket
+
+        for i in range(pow(2, global_depth)):
+            b = Bucket(global_depth)
+            self.buckets.append(b)
+            self.dict[i] = b
