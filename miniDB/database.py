79cfa642946b0a6d54521b6e23cbf8d181052a13
Apostolis2002 <apostolissiabanis@hotmail.com>
2023-02-08 17:44:34 +0200

    create_index pk_indx and unique_idx check

--- miniDB/database.py
+++ miniDB/database.py
@@ -667,8 +667,8 @@ class Database:
             table_name: string. Table name (must be part of database).
             index_name: string. Name of the created index.
         '''
-        #if self.tables[table_name].pk_idx is None or self.tables[table_name].unique_idx is None: # if no primary key, no index
-        #    raise Exception('Cannot create index. Table has no primary key or unique columns.')
+        if self.tables[table_name].pk_idx is None and self.tables[table_name].unique_idx is None: # if no primary key or any unique column, no index
+            raise Exception('Cannot create index. Table has no primary key or unique columns.')
         if index_name not in self.tables['meta_indexes'].column_by_name('index_name'):
             # currently only btree is supported. This can be changed by adding another if.
             if index_type=='btree':
