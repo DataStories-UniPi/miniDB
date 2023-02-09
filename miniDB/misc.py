ba3478c90e66fb9b3c6c1dfafe09b678649064b1
george202002 <giorgosapostolou33@gmail.com>
2023-02-09 19:40:37 +0200

    add comments for question 1

--- miniDB/misc.py
+++ miniDB/misc.py
@@ -45,12 +45,16 @@ def split_condition(condition):
             return left, op_key, right
 
 
+
 def check_logops(condition):
-    log_ops = {'between ',
+    '''
+    Checks the condition for logic operators, removes them and returns them
+    '''
+    log_ops = ['between ',
                'not ',
                ' and',
                ' or'
-               }
+               ]
 
     for log_op in log_ops:
         logsplt = condition.split(log_op)
