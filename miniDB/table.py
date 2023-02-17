04c558c977abddd3d762fd914e471e50cd275ca4
EktorasF <p20200@cslab.unipi.gr>
2023-02-17 13:24:50 +0200

    fixed previus commit

--- miniDB/table.py
+++ miniDB/table.py
@@ -288,6 +288,20 @@ class Table:
                 column = self.column_by_name(column_name)
                 operatorNOT = reverse_op(operator)
                 rows = [ind for ind, x in enumerate(column) if get_op(operatorNOT, x, value)]
+
+                if operator == "=":
+                    operatorNOT1 = "<"
+                    operatorNOT2 = ">"
+                
+                    rows1 = [ind for ind, x in enumerate(column) if get_op(operatorNOT1, x, value)]
+                    rows2 = [ind for ind, x in enumerate(column) if get_op(operatorNOT2, x, value)]
+                    rows3 = [rows1,rows2]
+                    rows = []
+                    for i in rows3:
+                        for j in i:
+                            if not (j in rows):
+                                rows.append(j)
+
             else:
                 column_name, operator, value = self._parse_condition(condition)
                 column = self.column_by_name(column_name)
@@ -380,6 +394,54 @@ class Table:
             s_table.data = [row for row in s_table.data if row is not None][:int(limit)]
 
         return s_table
+
+    def _select_where_with_hash(self, return_columns, hashT, condition, distinct=False, order_by=None, desc=True, limit=None):
+        if return_columns == '*':
+            return_cols = [i for i in range(len(self.column_names))]
+        else:
+            return_cols = [self.column_names.index(colname) for colname in return_columns]
+
+        column_name, operator, value = self._parse_condition(condition)
+
+        if column_name != self.column_names[self.pk_idx]:
+            print('Column is not PK. Aborting')
+            rows = []
+            # hash value
+            sum = 0
+            for l in value:
+                sum += ord(l)
+
+            hash_index = sum % int(hashT[0][0][0])
+
+            # find in dictionary with hash_index
+            for i in hashT[hash_index]:
+                if hashT[hash_index][i][1] == value:
+                    rows.append(hashT[hash_index][i][0])
+
+        try:
+            k = int(limit)
+        except TypeError:
+            k = None
+         # same as simple select from now on
+        rows = rows[:k]
+        # TODO: this needs to be dumbed down
+        dict = {(key):([[self.data[i][j] for j in return_cols] for i in rows] if key=="data" else value) for key,value in self.__dict__.items()}
+
+        dict['column_names'] = [self.column_names[i] for i in return_cols]
+        dict['column_types']   = [self.column_types[i] for i in return_cols]
+
+        s_table = Table(load=dict)
+
+        s_table.data = list(set(map(lambda x: tuple(x), s_table.data))) if distinct else s_table.data
+
+        if order_by:
+            s_table.order_by(order_by, desc)
+
+        if isinstance(limit,str):
+            s_table.data = [row for row in s_table.data if row is not None][:int(limit)]
+
+        return s_table
+
 
     def order_by(self, column_name, desc=True):
         '''
