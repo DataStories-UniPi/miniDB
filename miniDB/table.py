7cd28dd896832065d12bf29dc63060269a8667d1
georgesyrigos <georgesirigosaek@gmail.com>
2023-02-18 20:38:12 +0200

    Not added

--- miniDB/table.py
+++ miniDB/table.py
@@ -6,7 +6,7 @@ import sys
 
 sys.path.append(f'{os.path.dirname(os.path.dirname(os.path.abspath(__file__)))}/miniDB')
 
-from misc import get_op, split_condition
+from misc import get_op, split_condition,  reverse_op
 
 
 class Table:
@@ -233,9 +233,17 @@ class Table:
         # if condition is None, return all rows
         # if not, return the rows with values where condition is met for value
         if condition is not None:
-            column_name, operator, value = self._parse_condition(condition)
-            column = self.column_by_name(column_name)
-            rows = [ind for ind, x in enumerate(column) if get_op(operator, x, value)]
+            # not
+            if "NOT" in condition.split() or "not" in condition.split():
+                con_lst = condition.split("NOT")
+                con_lst = con_lst[0].split("not")
+                column_name, operator, value = self._parse_condition(
+                    con_lst[1])
+                column = self.column_by_name(column_name)
+                sec_op = reverse_op(operator)
+                rows = [ind for ind, x in enumerate(
+                    column) if get_op(sec_op, x, value)]
+            
         else:
             rows = [i for i in range(len(self.data))]
 
