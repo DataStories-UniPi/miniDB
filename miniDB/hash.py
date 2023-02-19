3065336c4b2dafbb792bd3c1a37de2a9f454e49a
irosolonaki <79923086+irosolonaki@users.noreply.github.com>
2023-02-19 16:10:07 +0200

    Code clean up and comments

--- miniDB/hash.py
+++ miniDB/hash.py
@@ -1,11 +1,21 @@
 BUCKET_SIZE = 20 # size of internal array in bucket
 
 class Bucket:
+    '''
+    Bucket Class is used by HashTable class to create/split buckets 
+    values in buckets are saved in lists called maps.
+    '''
     def __init__(self):
         self.map = []
         self.local_depth_size = 0
 
     def insert(self, k, v):
+        '''
+        Inserts item into the bucket
+        Args:
+            k : pointer.
+            v : string. the inserted value
+        '''
         for i, (key, value) in enumerate(self.map):
             if key == k:
                 del self.map[i]
@@ -13,6 +23,9 @@ class Bucket:
         self.map.append((k, v))
 
     def get(self, k):
+        '''
+        Get a value from the bucket
+        '''
         for key, value in self.map:
             if key == k:
                 return value
@@ -21,14 +34,20 @@ class Bucket:
       return 1 << self.local_depth # MSB variant for extendible hashing
 
 class HashTable:
+    '''
+    HashTable Class
+    Uses Extendible Hashing based on the MSB variant.    
+    '''
     def __init__(self):
-        self.global_depth_size = 0
+        self.global_depth_size = 0 # number of bits that will be used for splitting buckets
         self.directory = [Bucket()]
         self.capacity = BUCKET_SIZE
+        
 
     def get_bucket(self, k):
         h = hash(k)
         return self.directory[h & ((1 << self.global_depth_size) - 1)] # shift left by global_depth_size bits
+
 
     def hash(self, key):
         hashsum = 0
@@ -47,7 +66,7 @@ class HashTable:
                 self.directory *= 2
                 self.global_depth_size += 1
 
-            b0 = Bucket() # split into two bucket objects
+            b0 = Bucket() # split into bucket objects
             b1 = Bucket()
             b0.local_depth_size = b1.local_depth_size = b.local_depth_size + 1
             MSB = b.get_MSB() # get MSB variant
@@ -60,6 +79,11 @@ class HashTable:
                 self.directory[i] = b1 if i & MSB else b0
 
     def get(self, k):
+        '''
+        Returns the index of the value. If not found, returns None       
+        Args:
+            key: hashed value of the searched item.
+        '''
         return self.get_bucket(k).get(k)
 
 
