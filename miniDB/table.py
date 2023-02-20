3029ee2de6477bcd98bbcaec652c019649aa4775
Iraklis Memos <iramem18@gmail.com>
2023-02-20 21:24:26 +0200

    correction_2

--- miniDB/table.py
+++ miniDB/table.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from miniDB.misc import get_op, split_condition,  reverse_op
+from miniDB.misc import get_op, split_condition, reverse_op
 from tabulate import tabulate
 import pickle
 import os
